db2bff0e36f46216be21bdb56d504af0caf07e31
Anton Yevchenko <126160264+AntonYevchenko@users.noreply.github.com>
2023-11-10 17:11:04 +0200

    Add files via upload

---
+++ Task2.2.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Metrics;
+
+class Task
+{
+    public static void Main()
+    {
+        double input = double.Parse(Console.ReadLine());   
+
+        string str = input.ToString().Split(',')[1];
+
+        int result = (str[0] - '0') + (str[1] - '0');
+
+
+        //Second variant
+        result = input.ToString().Split(',')[1].Take(2).Sum(c => c - '0'); 
+
+    }
+}
