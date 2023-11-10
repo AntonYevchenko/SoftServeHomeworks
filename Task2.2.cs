5ad7c9ab82902ef77c65290251fa789a0202b507
Anton Yevchenko <126160264+AntonYevchenko@users.noreply.github.com>
2023-11-10 17:14:39 +0200

    Update Task2.2.cs

--- Task2.2.cs
+++ Task2.2.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Metrics;
+//Enter double  number  and get the first 2 digits after the point of this number and output the sum of these numbers.
+//For example: 3.456 -> 4+5=9
 
 class Task
 {
