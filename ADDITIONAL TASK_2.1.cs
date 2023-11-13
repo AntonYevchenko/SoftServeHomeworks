b343c74fa08d36cdb5ff9d724ede8d7825fcf711
Anton Yevchenko <126160264+AntonYevchenko@users.noreply.github.com>
2023-11-13 19:42:21 +0200

    Add files via upload

---
+++ ADDITIONAL TASK_2.1.cs
@@ -0,0 +1,29 @@
+﻿/*Create class Cat. The cat should have a property "fullness level" and method "eat something". Create enum Food (fish, mouse, ...). Each type of food should change the level of satiety differently.*/
+
+namespace SoftServeHomeWorks
+{
+    internal class ADDITIONAL_TASKS
+    {
+        public enum Food
+        {
+            Fish = 1,
+            Mouse,
+            StolenMeat,
+            Candy = -2
+        }
+        private class Cat
+        {
+            public int FullnessLevel { get; private set; } = 0;
+
+            public void EatSomething(Food food) =>
+                FullnessLevel += (int) food;
+        }
+        public static void Main(string[] args)
+        {
+            var food = Food.Mouse;
+            var cat = new Cat();
+            cat.EatSomething(food);
+            Console.WriteLine(cat.FullnessLevel);
+        }
+    }
+}
---
+++ Task_3.4.cs
@@ -0,0 +1,44 @@
+﻿/*Enter a sequence of positive integers (to the first negative). Calculate the arithmetic average of the entered positive numbers. //1,6,3,9,-8 -> (1+6+3+9)/4*/
+
+namespace SoftServeHomeWorks
+{
+    internal class Task_3
+    {
+        public static void Main(string[] args)
+        {
+            int positiveCount = 0;
+
+            int accamulate = 0;
+
+            while (true)
+            {
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < 0)
+                    break;
+
+                else
+                {
+                    positiveCount++;
+                    accamulate += userInput;
+                }
+            }
+            Console.WriteLine((double) accamulate / positiveCount);
+
+
+
+            var accamulate2 = new List<int>();
+
+            while (true)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (input < 0)
+                    break;
+                else 
+                    accamulate2.Add(input);
+            }
+            Console.WriteLine((double) accamulate2.Sum() / accamulate2.Count);
+        }
+    }
+}
